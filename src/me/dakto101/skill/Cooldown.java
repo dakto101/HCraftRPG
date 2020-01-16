b9fa9ec745bb2d7aad7b028e4b9edf437865ddf8
hcraft <36808098+dakto101@users.noreply.github.com>
2020-01-16 12:40:45 +0700

    Revert "Initial commit"
    This reverts commit 3ece5039907dda53e0a63930266f011864cd198c.

--- src/me/dakto101/skill/Cooldown.java
+++
@@ -1,9 +0,0 @@
-package me.dakto101.skill;
-
-public interface Cooldown {
-
-	void startCooldown(Object o);
-	long getRemainingCooldown(Object o);
-	boolean onCooldown(Object o);
-	
-}
--- src/me/dakto101/skill/SkillAttribute.java
+++
@@ -1,5 +0,0 @@
-package me.dakto101.skill;
-
-public class SkillAttribute {
-	//Update sau khi lam MySQL RPGPlayer
-}
