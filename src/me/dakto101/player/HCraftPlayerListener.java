15e2de22eda1ffab0531176476692f353c099d47
hcraft <36808098+dakto101@users.noreply.github.com>
2020-01-18 22:02:20 +0700

    18/01/20 update

--- src/me/dakto101/player/HCraftPlayerListener.java
+++ src/me/dakto101/player/HCraftPlayerListener.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -15,23 +16,43 @@ import org.bukkit.scheduler.BukkitScheduler;
 import me.dakto101.HCraftRPG;
 
 public class HCraftPlayerListener implements Listener {
-	
+	//Check player online
+	//Auto save player data.
+
 	private static List<HCraftPlayer> playerList = new ArrayList<HCraftPlayer>();
 	
 	@EventHandler
 	public void onEnable(PluginEnableEvent e) {
+		//Check plugin
+		if (!e.getPlugin().getName().equals(HCraftRPG.plugin.getName())) return;
 		playerList = new ArrayList<HCraftPlayer>();
+		
         BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
+        //Timer task (1.5s): Init list and add all player to list
+        scheduler.runTaskLater(HCraftRPG.plugin, new Runnable() {
+        	@Override
+        	public void run() {
+        		for (Player player : Bukkit.getServer().getOnlinePlayers()) {
+        			playerList = new ArrayList<HCraftPlayer>();
+        			playerList.add(new HCraftPlayer(player));
+        		}
+        	}
+        }, 20L);
+        //Repeat task: Remove player if offline (600s each check)
         scheduler.scheduleSyncRepeatingTask(HCraftRPG.plugin, new Runnable() {
             @Override
             public void run() {
-                // Do something
+                for (int i = 0; i < playerList.size(); i++) {
+                	if (!playerList.get(i).getPlayer().isOnline()) playerList.remove(i);
+                }
             }
-        }, 0L, 20L);
+        }, 100L, 600L*20);
+
 	}
-	
 	@EventHandler
 	public void onDisable(PluginDisableEvent e) {
+		if (!e.getPlugin().getName().equals(HCraftRPG.plugin.getName())) return;
+		
 		for (HCraftPlayer p : playerList) {
 			p.saveData();
 		}
